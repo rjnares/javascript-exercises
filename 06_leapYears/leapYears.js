e58df5df9b3c529e98657aeb5d192b0fba6eeb13
Roberto Nares <roberto.nares@gmail.com>
2024-06-24 21:50:15 -0700

    Add answer for exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divByFour = year % 4 == 0;
+    const divByOneHundred = year % 100 == 0;
+    const divByFourHundred = year % 400 == 0;
 
+    return divByFour && (!divByOneHundred || divByFourHundred);
 };
 
 // Do not edit below this line
