3f52f0162d6aabde0f30d7d36cc1a2a17dfcbf01
Roberto Nares <roberto.nares@gmail.com>
2024-06-24 20:52:07 -0700

    Add answer for exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let reversedString = "";
 
+    for (let idx = input.length - 1; idx >= 0; idx--) {
+        reversedString += input.at(idx);
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
