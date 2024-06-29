68193d84fb34a6b7d0a31cc14c3f2b9f5271b162
Roberto Nares <roberto.nares@gmail.com>
2024-06-29 13:06:52 -0700

    Add answers for exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (typeof num !== "number") {
+        num = parseInt(num);
+    }
 
+    if (num < 0) return "OOPS";
+    if (num < 2) return num;
+    
+    let twoAgo = 0;
+    let oneAgo = 1;
+    
+    for (let n = 2; n <= num; n++) {
+        oneAgo += twoAgo;
+        twoAgo = oneAgo - twoAgo;
+    }
+    return oneAgo;
 };
 
 // Do not edit below this line
