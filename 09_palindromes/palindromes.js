cbd3d542642a0ace0554de8deaed9d1aaeb86004
Roberto Nares <roberto.nares@gmail.com>
2024-06-29 12:50:01 -0700

    Add answers for exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumericChars = "abcdefghijklmnopqrstuvwxyz0123456789"
 
+    const arr = str
+        .toLowerCase()
+        .split("")
+        .filter((char) => alphanumericChars.includes(char))
+
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        if (arr[left] !== arr[right]) return false;
+        left++;
+        right--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
